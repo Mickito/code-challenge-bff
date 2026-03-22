55fee20c5c0ac691509dd8c23e547399f7ea9077
Mickito <micksayboualay@gmail.com>
2026-03-22 13:16:32 +1100

    Code cleanup and polish

--- tests/rules/bulk-discount.test.ts
+++ tests/rules/bulk-discount.test.ts
@@ -34,13 +34,13 @@ describe("BulkDiscount", () => {
     );
   });
 
-  test("should throw error when discount threshold below 0", () => {
+  test("should throw error when discount threshold is below 0", () => {
     expect(() => new BulkDiscount(catalog.ipd.sku, -1, 123.45)).toThrow(
       "discount threshold must be greater than 0"
     );
   });
 
-  test("should throw error when new price below 0", () => {
+  test("should throw error when new price is below 0", () => {
     expect(() => new BulkDiscount(catalog.ipd.sku, 2, -123.45)).toThrow(
       "new price must be greater than 0"
     );
@@ -49,7 +49,7 @@ describe("BulkDiscount", () => {
   test.each([
     ["above current price", catalog[catalog.ipd.sku].price + 1234],
     ["equal to current price", catalog[catalog.ipd.sku].price],
-  ])("should throw error when new price %s", (_, newPrice) => {
+  ])("should throw error when new price is %s", (_, newPrice) => {
     expect(() => new BulkDiscount(catalog.ipd.sku, 2, newPrice)).toThrow(
       "new price must be lower than current selected item price"
     );
